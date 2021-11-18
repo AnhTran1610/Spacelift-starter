250f6102014f46284b5aebe521e7352779701805
AnhTran1610 <theanh161093@gmail.com>
2021-11-18 22:10:52 +0700

    test

--- main.tf
+++ main.tf
@@ -14,11 +14,36 @@ terraform {
 
 resource "spacelift_stack" "this" {
   administrative    = true
-  autodeploy        = true
+  autodeploy        = false
   branch            = "master"
   description       = "Testing workflow"
   name              = "Spacelift-starter"
   repository        = "spacelift-starter"
   terraform_version = "1.0.5"
-  project_root = "managed-stack"
+}
+
+resource "spacelift_environment_variable" "stack-plaintext" {
+  stack_id   = spacelift_stack.this.id
+  name       = "TF_VAR_stack_public"
+  value      = "This should be visible!"
+  write_only = false
+}
+
+# For another (secret) variable, let's create programmatically create a super
+# secret password.
+
+
+# This is a secret environment variable. Note how we didn't set the write_only
+# bit at all here. This setting always defaults to "true" to protect you against
+# an accidental leak of secrets. There will be no way to retrieve the value of
+# this variable programmatically, but it will be available to your Spacelift
+# runs.
+#
+# If you accidentally print it out to the logs, no worries: we will obfuscate
+# every secret thing we know of.
+resource "spacelift_environment_variable" "stack-writeonly" {
+  stack_id = spacelift_stack.this.id
+  name     = "TF_VAR_stack_secret"
+  value    = "123456"
+  write_only = false
 }
