0a066565a0250ce6311c67e910fea1b91aea96d3
AnhTran1610 <theanh161093@gmail.com>
2021-11-18 22:33:27 +0700

    test

--- managed-stack/first-stack.tf
+++
@@ -1,9 +0,0 @@
-resource "spacelift_stack" "first-stack" {
-  administrative    = false
-  autodeploy        = false
-  branch            = "master"
-  description       = "Testing workflow"
-  name              = "QUICK LAB"
-  repository        = "quick_lab"
-  terraform_version = "1.0.5"
-}
