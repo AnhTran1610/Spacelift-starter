661e93c410f93c640dfdd5c3b066fb45d1905b9f
Thế Anh Trần <theanh161093@gmail.com>
2021-11-24 16:43:10 +0700

    Update main.tf

--- tf-code/main.tf
+++ tf-code/main.tf
@@ -13,7 +13,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-049c1c743feb6f504"
   instance_type = "t2.micro"
 
   tags = {
