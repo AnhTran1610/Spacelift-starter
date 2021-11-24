05d6f4419f5e1659e2219e0849879fb7ab486ce7
Thế Anh Trần <theanh161093@gmail.com>
2021-11-24 17:06:35 +0700

    Update main.tf

--- tf-code/main.tf
+++ tf-code/main.tf
@@ -11,12 +11,3 @@ terraform {
 provider "aws" {
   region  = "ap-southeast-1"
 }
-
-resource "aws_instance" "app_server" {
-  ami           = "ami-049c1c743feb6f504"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "ExampleAppServerInstance"
-  }
-}
